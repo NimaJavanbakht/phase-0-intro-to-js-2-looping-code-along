a2cf825a4e28f002cc3e83fe064c56464d020d03
Nima Javanbakht <nimajavan230@gmail.com>
2023-06-20 16:50:29 -0700

    completes looping lab

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+let myArray = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards() {
+    let cardNames = []
+    for (let i = 0; i < myArray.length; i++) {
+    cardNames.push(`Thank you, ${myArray[i]}, for the wonderful surprise gift!`)
+    }
+    return cardNames;
+}
+
+function countDown() {
+    let downCount = 10;
+    while (downCount <= 10 && downCount >= 0) {
+        console.log(downCount--)
+    }
+    return downCount;
+}
